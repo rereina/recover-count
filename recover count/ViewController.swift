5f156861fc7001e80e2274a9d2af2098cb6d0437
佐藤　玲奈 <rerepi5834coo@gmail.com>
2015-08-15 07:08:01 +0900

    Initial Commit

---
+++ recover count/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  recover count
+//
+//  Created by 佐藤　玲奈 on 2015/08/15.
+//  Copyright (c) 2015年 佐藤　玲奈. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
